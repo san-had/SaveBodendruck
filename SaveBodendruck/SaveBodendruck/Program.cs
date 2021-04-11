d44d0ed2a1fb9a9d152f038e04932172bd5f1aa7
san-had <hadhazis@gmail.com>
2021-04-11 12:34:28 +0200

    Add empty project

---
+++ SaveBodendruck/SaveBodendruck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaveBodendruck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
